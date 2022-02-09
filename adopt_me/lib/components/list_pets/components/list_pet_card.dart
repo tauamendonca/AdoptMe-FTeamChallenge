3b74231489519a3cec4c4de7987f818ecb241776
Tauã Mendonça <tauamendonca@gmail.com>
2022-02-08 21:54:40 -0300

    <done> Heart button - <ToDo> 2nd Screen

--- adopt_me/lib/components/list_pets/components/list_pet_card.dart
+++ adopt_me/lib/components/list_pets/components/list_pet_card.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, prefer_typing_uninitialized_variables
 
 import 'package:adoptme/components/constants.dart';
+import 'package:adoptme/components/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
 
 class ListPetCard extends StatefulWidget {
@@ -31,26 +32,27 @@ class ListPetCard extends StatefulWidget {
 /   defeito bem grande, que é o de limitar o tamanho do leading, aí a foto do card
 /   nunca estaria no tamanho correto. A solução foi criar um card customizado. 
 */
+
 class _ListPetCardState extends State<ListPetCard> {
   //controle de qual ícone será recebido de acordo com o petFavorite
   //petFavorite = true se o pet for favoritado
+  late bool favorite = false;
+  late var _heart;
+  late var _heartColor;
 
-  //TODO: FAZER OS CORAÇÕES
-  // late bool _heart;
-  // late var _heartColor;
-  // getData(_heart) {
-  //   if (widget.petFavorite) {
-  //     _heart = true;
-  //     _heartColor = cPrimaryColor;
-  //   } else {
-  //     _heart = false;
-  //     _heartColor = cGray2;
-  //   }
-  // }
+  getData(favorite) {
+    if (widget.petFavorite) {
+      _heart = CustomIcons.favorite;
+      _heartColor = cPrimaryColor;
+    } else {
+      _heart = CustomIcons.favorite_border;
+      _heartColor = cGray2;
+    }
+  }
 
   @override
   void initState() {
-    // getData(widget.petFavorite);
+    getData(widget.petFavorite);
     super.initState();
   }
 
@@ -60,83 +62,105 @@ class _ListPetCardState extends State<ListPetCard> {
 
     //container que determina o tamanho do card
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.40,
-      height: MediaQuery.of(context).size.height * 0.165,
+        width: MediaQuery.of(context).size.width * 0.40,
+        height: MediaQuery.of(context).size.height * 0.165,
 
-      //Card
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        color: cSecondaryColor,
-        elevation: 0,
+        //Card
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          color: cSecondaryColor,
+          elevation: 0,
 
-        //linha do card, os itens dela estão separados em 3 widgets
-        child: Row(
-          children: [
-            //Stack com a foto do pet e o fundo
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 15, 10),
-              child:
-                  //fundo da foto
-                  Container(
-                width: MediaQuery.of(context).size.width * 0.24,
-                height: MediaQuery.of(context).size.height * 0.16,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(22),
-                  color: Color(int.parse(widget.petColor)),
-                ),
+          //linha do card, os itens dela estão separados em 3 widgets
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 15, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                //Stack com a foto do pet e o fundo
 
-                //foto do pet
-                child: Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(22)),
-                  child: Image.asset(widget.petProfilePic.toString()),
-                ),
-              ),
-            ),
+                //fundo da foto
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.24,
+                  height: MediaQuery.of(context).size.height * 0.16,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(22),
+                    color: Color(int.parse(widget.petColor)),
+                  ),
 
-            //Textos
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: Text(
-                    widget.petName,
-                    overflow: TextOverflow.ellipsis,
-                    style: cTitleStyle,
+                  //foto do pet
+                  child: Container(
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(22)),
+                    child: Image.asset(widget.petProfilePic.toString()),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                  child: Text(
-                    widget.petRace,
-                    overflow: TextOverflow.ellipsis,
-                    style: cSubtitleStyle,
-                  ),
+
+                const SizedBox(
+                  width: 10,
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                  child: Text(
-                    "${widget.petSex} , ${widget.petAge}",
-                    overflow: TextOverflow.ellipsis,
-                    style: cBaseTextStyle,
-                  ),
+                //Textos
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: Text(
+                        widget.petName,
+                        overflow: TextOverflow.ellipsis,
+                        style: cTitleStyle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                      child: Text(
+                        widget.petRace,
+                        overflow: TextOverflow.ellipsis,
+                        style: cSubtitleStyle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                      child: Text(
+                        "${widget.petSex} , ${widget.petAge}",
+                        overflow: TextOverflow.ellipsis,
+                        style: cBaseTextStyle,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 14, 0, 0),
+                        child: Row(
+                          children: [
+                            const Icon(CustomIcons.location,
+                                size: 18, color: cPrimaryColor),
+                            Text(
+                              "${widget.petLocation} kms away",
+                              overflow: TextOverflow.ellipsis,
+                              style: cBaseTextStyle,
+                            ),
+                          ],
+                        )),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                  child: Text(
-                    "${widget.petLocation} kms away",
-                    overflow: TextOverflow.ellipsis,
-                    style: cBaseTextStyle,
+
+                const SizedBox(
+                  width: 50,
+                ),
+
+                //Ícone de Coração
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Icon(
+                    _heart,
+                    color: _heartColor,
+                    size: 30,
                   ),
                 ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
